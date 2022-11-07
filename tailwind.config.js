f8cba45c81b3ea4de37e7987f4b57e510cd181f1
Amit Kumar Dubey <tech.admin@ardouranalytics.com>
2022-11-07 23:39:32 +0530

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: '#025196',
+        accent: '#fdb338'
+      },
     },
   },
   plugins: [],
