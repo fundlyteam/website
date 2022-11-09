613a0a066bbf814175b0022b857e73d93db50dc5
Amit Kumar Dubey <tech.admin@ardouranalytics.com>
2022-11-09 12:47:39 +0530

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,11 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    fontFamily: {
-      brand: ['Lato', 'sans-serif']
-    },
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        brand: ['Lato', 'sans-serif'],
+        script: ['Kalam', 'cursive'],
       },
       colors: {
         primary: '#025196',
