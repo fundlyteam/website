325ba0ba8329bd75a639db2953cbe197c11c07e8
Amit Kumar Dubey <tech.admin@ardouranalytics.com>
2022-11-13 22:31:48 +0530

    wip

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import Swiper, {Pagination, Scrollbar, Navigation, Autoplay} from 'swiper';
+import anime from "animejs";
 import 'swiper/swiper-bundle.min.css';
 
 const benefitSwiper = new Swiper('#benefit-swiper', {
@@ -33,39 +34,110 @@ const testimonialSwiper = new Swiper('#testimonial-swiper', {
     },
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-    const dismissingButtons = document.querySelectorAll('[data-dismiss]');
-    dismissingButtons.forEach((button) => {
-        button.addEventListener('click', (e) => {
-            e.stopImmediatePropagation();
-            const toggleable = e.target.closest('[data-toggleable]')
-            toggleable.classList.toggle('hidden');
-        })
+function handleMobileNav() {
+    const showMobileNav = document.getElementById('show-mobile-nav');
+    const hideMobileNav = document.getElementById('hide-mobile-nav');
+    const mobileNav = document.getElementById('mobile-nav')
+    showMobileNav.addEventListener('click', () => {
+        mobileNav.classList.remove('pointer-events-none');
+        anime({
+            targets: '#mobile-nav',
+            height: "100vh",
+            opacity: 1,
+            duration: 200,
+            easing: 'linear',
+        });
     })
+    hideMobileNav.addEventListener('click', (e) => {
+        mobileNav.classList.add('pointer-events-none');
+        anime({
+            targets: '#mobile-nav',
+            keyframes: [
+                {height: 0, duration: 100},
+                {opacity: 0, duration: 0}
+            ],
+            easing: 'linear',
+        });
+    })
+}
 
-    const toggleButtons = document.querySelectorAll('[data-toggle]');
-    toggleButtons.forEach((button) => {
-        button.addEventListener('click', (e) => {
-            e.stopImmediatePropagation();
-            const clickedButton = e.target.closest('[data-toggle]');
-            const target = document.getElementById('mobile-nav');
-            if (target.classList.contains('hidden')) {
-                target.classList.add('animate-slideInRight');
-                target.classList.remove('hidden');
-                target.classList.remove('animate-slideOutRight');
-            } else {
-                target.classList.remove('animate-slideInRight');
-                target.classList.add('animate-slideOutRight');
-                target.classList.add('hidden');
-            }
-            // if (target.classList.contains('hidden')) {
-            //     target.classList.remove('hidden');
-            // } else {
-            //     target.classList.add('hidden');
-            // }
+function handleDownloadAppModal() {
+    const showDownloadAppModal = document.querySelectorAll('.show-download-app-modal');
+    const hideDownloadAppModal = document.querySelectorAll('.hide-download-app-modal');
+    const downloadAppModal = document.getElementById('download-app-modal');
 
-            target.addEventListener('animationend', () => {
+    showDownloadAppModal.forEach(handler => {
+        handler.addEventListener('click', () => {
+            downloadAppModal.classList.remove('hidden');
+            anime({
+                targets: '#body',
+                opacity: 100,
+                translateY: 0,
+                scale: 1,
+                duration: 300,
+                easing: 'easeOutSine'
+
+            })
+            anime({
+                targets: '#backdrop',
+                opacity: 100,
+                duration: 300,
+                easing: 'easeOutSine',
+                complete: function () {
+                    downloadAppModal.querySelector('input:first-child').focus()
+                }
             })
         })
     })
+
+    hideDownloadAppModal.forEach(handler => {
+        handler.addEventListener('click', () => {
+            anime({
+                targets: '#body',
+                opacity: 0,
+                translateY: "16px",
+                scale: 0.95,
+                duration: 200,
+                easing: 'easeInSine',
+            })
+            anime({
+                targets: '#backdrop',
+                opacity: 100,
+                duration: 200,
+                easing: 'easeInSine',
+                complete: function () {
+                    downloadAppModal.classList.add('hidden')
+                }
+            })
+        })
+    })
+}
+
+function handleDownloadAppForm() {
+    const downloadAppForm = document.getElementById('send-app-link');
+    downloadAppForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const mobile = document.getElementById('mobile');
+        if (mobile.validity.patternMismatch) {
+            mobile.setCustomValidity("Please enter your ten digit mobile number.");
+            mobile.reportValidity();
+        } else {
+            mobile.setCustomValidity("");
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    handleMobileNav();
+    handleDownloadAppModal();
+    handleDownloadAppForm();
+
+    //     Modal panel, show/hide based on modal state.
+    //
+    //     Entering: "ease-out duration-300"
+    // From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    // To: "opacity-100 translate-y-0 sm:scale-100"
+    // Leaving: "ease-in duration-200"
+    // From: "opacity-100 translate-y-0 sm:scale-100"
+    // To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 })
